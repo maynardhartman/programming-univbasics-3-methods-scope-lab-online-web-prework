cc6e6fe5e3dfd2cc66712936b15353a7bada2aef
Maynard Hartman <mjhartman@brighthouse.com>
2019-08-27 23:22:29 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,6 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = 'Thank You status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  pharse = 'Mario! But Our Princess Is In Another Castle!'
   puts phrase
   phrase = "It's-a me, Mario!"
 end
